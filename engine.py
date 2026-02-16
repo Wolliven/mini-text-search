2ceb81478527458d1e3405d6cef2c8e90df2ff4e
Jerico San Nicolas <jericosnicolas@gmail.com>
2026-02-16 21:08:33 +0900

    Add single txt file use case

--- engine.py
+++ engine.py
@@ -1,10 +1,10 @@
 #The main engine for the mini-text-search project. Handles indexing and searching functionality.
-from utils import normalize, tokenize
+from utils import tokenize
 import sys
 import json
 from pathlib import Path
 
-def analyze_file(path):
+def tokenize_file(path):
     try:
         with open(path, 'r', encoding='utf-8') as f:
             content = tokenize(f.read())
@@ -16,31 +16,35 @@ def analyze_file(path):
         print(f"An error occurred while processing the file '{path}': {e}")
         return None
 
+def analyze_file(file_path, results):
+    results["total_files"] += 1
+    results["analyzed_files"].append(str(file_path))
+    local_result = tokenize_file(file_path)
+    for local_word in local_result:
+        results["index"].setdefault(local_word, []).append(str(file_path))
+
 def build_index(path):
     general_result = {
         "total_files": 0,
         "analyzed_files": [],
         "index": {}
         }
-    temp_index = {}
 
     p = Path(path)
     if not p.exists():
-        print(f"The folder '{path}' does not exist.")
+        print(f"The folder or file '{path}' does not exist.")
         sys.exit(1)
-    if not p.is_dir():
-        print(f"'{path}' is not a folder.")
+    if p.is_dir():
+        for file_path in Path(path).rglob("*.txt"):
+            analyze_file(file_path, general_result)
+
+    elif p.is_file() and p.suffix == ".txt":
+        analyze_file(p, general_result)
+    else:
+        print(f"'{path}' is not a valid file or folder.")
         sys.exit(1)
-
-    for file_path in Path(path).rglob("*.txt"):
-        general_result["total_files"] += 1
-        general_result["analyzed_files"].append(str(file_path))
-        local_result = analyze_file(file_path)
-        for local_word in local_result:
-            general_result["index"][local_word] = general_result["index"].get(local_word, []) + [str(file_path)]
-
     print(json.dumps(general_result, indent=4))
+
 
 def search(query):
     print(f"Searching for query: {query}")
-
